b3faed799c239464ec5d3f62ec5f4692005796f6
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-01-11 13:19:22 -0300

    agregando efecto de vidrio

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,18 +2,15 @@
 export const AboutMe = () => {
 
   return (
-    <div className="bg-white p-5 shadow rounded-md">
+    <div className="bg-transparent border-4 border-gray-100 backdrop-blur-sm p-5 shadow rounded text-white">
       <img
-        className="w-44 shadow rounded-full mx-auto"
+        className="w-44 shadow rounded-full mx-auto border-2 border-gray-100"
         src="/developer.jpg"
         alt="Nicolás Lopez Vidueiros" />
       <div className="flex flex-col items-center justify-center space-y-4">
         <h2 className="font-black text-5xl">Sobre Mí</h2>
-        <p className="text-lg leading-7 w-96 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae cumque
-          voluptas voluptates, voluptate voluptatem quidem, quod quos, quia quas
-          quibusdam quia quod quos, quia quas quibusdam quia quod quos, quia quas
-          quibusdam quia quod quos, quia quas quibusdam quia quod quos, quia quas
+        <p className="text-xl leading-7 w-96 text-center">
+          Me encanta mejorar mi lógica de programación y descubrir nuevas tecnologías para crear soluciones innovadoras. En mi tiempo libre, disfruto mirando anime, series y películas, que me inspiran con historias creativas y me ayudan a desconectar mientras exploro nuevas ideas. Siempre busco crecer tanto a nivel personal como profesional, enfrentando desafíos con entusiasmo y dedicación.
         </p>
       </div>
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ export const Footer = () => {
   const date = new Date().getFullYear()
 
   return (
-    <footer className="bg-slate-900 p-5">
-      <div className="max-w-5xl mx-auto text-white">
-        <p>Hecho con ❤️ por Nicolas Lopez Vidueiros | {date} - Todos los derechos reservados</p>
+    <footer className="bg-slate-900 p-10">
+      <div className="max-w-7xl mx-auto text-white">
+        <p className="text-lg">Hecho con ❤️ por Nicolas Lopez Vidueiros | {date} - Todos los derechos reservados</p>
       </div>
     </footer>
   )
