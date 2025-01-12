a6c23305ba14fc15f7f26eba16a534acb9121771
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-01-12 18:15:42 -0300

    eliminando importación innecesaria

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { AboutMe } from "./components/AboutMe";
-import { ContactMe } from "./components/ContactMe";
 import { Footer } from "./components/Footer";
 import Header from "./components/Header";
 import { Projects } from "./components/Projects";
